e2e7138a19322fe52330bb6df0ae2c2f6d2eada8
whattablackhole <rudolf.gritsenyak@gmail.com>
2024-10-15 15:19:42 +0300

    feat: add env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
