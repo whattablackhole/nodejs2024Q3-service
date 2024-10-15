a82dfe963ab3112e5d369248dbc0fb72f8cce4d5
whattablackhole <rudolf.gritsenyak@gmail.com>
2024-10-15 15:06:40 +0300

    init: add empty user module and app types

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { RouterModule } from '@nestjs/core/router';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    RouterModule.register([
+      {
+        path: 'user',
+        module: UserModule,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
